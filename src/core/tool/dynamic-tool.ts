148b790d9bbb2b59f19407884f75c62292722ab6
kiyonya <481366085@qq.com>
2026-05-18 16:53:34 +0800

    fix: 修复类型错误

--- src/core/tool/dynamic-tool.ts
+++ src/core/tool/dynamic-tool.ts
@@ -6,7 +6,7 @@ type PermissionChecker = () => boolean | Promise<boolean>
 type IPermissions = Record<string, PermissionChecker>
 type NonVoidToolResult<T> = T extends void ? never : T
 
-type GeneratedTool<ToolParams extends Record<string, any> = {}, ToolResult = NonVoidToolResult<any>, Permissions = IPermissions> = LocalTool<ToolParams, ToolResult, Permissions> | MCPTool<ToolParams, ToolResult>
+type GeneratedTool<ToolParams extends Record<string, any> = {}, ToolResult = NonVoidToolResult<any>, Permissions = IPermissions> = LocalTool<ToolParams, ToolResult> | MCPTool<ToolParams, ToolResult>
 
 export default class DynamicTool<ToolParams extends Record<string, any> = {}, ToolResult = NonVoidToolResult<any>, Permissions = IPermissions> {
     public toolName: string
@@ -25,7 +25,7 @@ export default class DynamicTool<ToolParams extends Record<string, any> = {}, To
         }
         return tool
     }
-    public static toDynamicTool<ToolParams extends Record<string, any> = {},ToolResult = NonVoidToolResult<any>,Permissions = IPermissions>(tool: LocalTool<ToolParams, ToolResult, Permissions> | MCPTool<ToolParams, ToolResult>): DynamicTool<ToolParams, ToolResult, Permissions> {
+    public static toDynamicTool<ToolParams extends Record<string, any> = {},ToolResult = NonVoidToolResult<any>,Permissions = IPermissions>(tool: LocalTool<ToolParams, ToolResult> | MCPTool<ToolParams, ToolResult>): DynamicTool<ToolParams, ToolResult, Permissions> {
         const name = tool.toolName
         return new this(name, () => tool)
     }
