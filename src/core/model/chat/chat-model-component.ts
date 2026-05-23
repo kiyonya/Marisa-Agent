9da84e212e9cb8c49ba34dda7102eb689a20dd62
kiyonya <481366085@qq.com>
2026-05-23 15:54:17 +0800

    fix: 不使用管道事件

--- src/core/model/chat/chat-model-component.ts
+++ src/core/model/chat/chat-model-component.ts
@@ -3,11 +3,13 @@ import EventEmitter from "node:events";
 import path from "node:path";
 import { Marisa } from "@type/marisa";
 import ChatModelComponentInstaller from "./chat-model-component-installer";
+import JustEventEmitter from "@core/utils/emitter";
+
 export default abstract class ChatModelComponent<T extends Record<keyof T, any[]>> extends EventEmitter<T> {
 
     public workspace: string = path.resolve('./workspace')
     protected injectModelTools: Marisa.Tool.AnyToolParam[] = []
-    public installFunction:((installer:ChatModelComponentInstaller,modelInfo:Marisa.Model.ChatModelInfo)=>void) | null = null
+    public installFunction: ((installer: ChatModelComponentInstaller, modelInfo: Marisa.Model.ChatModelInfo) => void) | null = null
     constructor() {
         super()
     }
