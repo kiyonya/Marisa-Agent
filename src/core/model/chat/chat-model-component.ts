50fbe654622fc637f17d24b34f5dd5a8e02678e2
kiyonya <481366085@qq.com>
2026-05-24 13:52:53 +0800

    fix: 移除旧引用

--- src/core/model/chat/chat-model-component.ts
+++ src/core/model/chat/chat-model-component.ts
@@ -3,7 +3,6 @@ import EventEmitter from "node:events";
 import path from "node:path";
 import { Marisa } from "@type/marisa";
 import ChatModelComponentInstaller from "./chat-model-component-installer";
-import JustEventEmitter from "@core/utils/emitter";
 
 export default abstract class ChatModelComponent<T extends Record<keyof T, any[]>> extends EventEmitter<T> {
 
