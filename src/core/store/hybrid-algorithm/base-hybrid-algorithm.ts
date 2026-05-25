34520ebd8f89ae671ee34007117b6a5f50647366
kiyonya <481366085@qq.com>
2026-05-25 19:09:38 +0800

    remove: 移除过时的算法

--- src/core/store/hybrid-algorithm/base-hybrid-algorithm.ts
+++
@@ -1,6 +0,0 @@
-import { HybridStoreQueryResult } from "../impl/result";
-
-export default abstract class BaseHybridAlgorithm {
-
-    public abstract run<Metadata = any>(vectorResult: HybridStoreQueryResult<Metadata>[], keywordResult: HybridStoreQueryResult<Metadata>[],limit:number,...args:any[]): HybridStoreQueryResult<Metadata>[] | Promise<HybridStoreQueryResult<Metadata>[]>
-}
--- src/core/store/hybrid-algorithm/hybrid-algorithm.ts
+++
@@ -1,6 +0,0 @@
-import ReciprocalRankFusion from "./reciprocal-rank-fusion";
-
-export default abstract class HybridAlgorithm {
-
-    public static ReciprocalRankFusion = ReciprocalRankFusion
-}
