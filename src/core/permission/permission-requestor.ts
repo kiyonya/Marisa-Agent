46f19b138f1beff266c0a01c16125020a36226f5
kiyonya <481366085@qq.com>
2026-05-25 19:11:17 +0800

    fix: 移除不使用的导入

--- src/core/permission/permission-requestor.ts
+++ src/core/permission/permission-requestor.ts
@@ -1,6 +1,4 @@
 
-import inquirer from "inquirer";
-
 export default abstract class PermissionAsker {
     public abstract askConfirm(message: string, defaultValue?: boolean ): Promise<boolean>
     protected gencid() {
