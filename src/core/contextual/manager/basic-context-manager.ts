ee472b47a66aa374cfcd2af11a773f2ab45578b0
kiyonya <481366085@qq.com>
2026-05-25 19:10:34 +0800

    feat: 更新基本上下文管理器以支持保存会话和改进put方法

--- src/core/contextual/manager/basic-context-manager.ts
+++ src/core/contextual/manager/basic-context-manager.ts
@@ -8,36 +8,45 @@ export interface BasicContextManagerOptions {
 
 export default class BasicContextManager extends ModelContextManager {
     private options?: BasicContextManagerOptions
-    private addContextFunction?:(session: Marisa.Chat.Completion.CompletionSession)=>void
-    constructor(options?: BasicContextManagerOptions) {
-        super()
+    private addContextFunction?: (session: Marisa.Chat.Completion.CompletionSession) => void
+    
+    constructor(options?: BasicContextManagerOptions, savedSessions?: Marisa.Chat.Completion.CompletionSession[]) {
+        super(savedSessions)
         this.options = options
+        
         this.installFunction = (installer) => {
+            this.loadContext(installer.getWorkspace('contexts'))
             installer.registerModelContextPutFunction(this.put.bind(this))
             installer.registerModelContextQueryFunction(this.query.bind(this))
             this.addContextFunction = this.createAddContextFunction(installer.getWorkspace('contexts'))
         }
     }
 
-    public async put(session: Marisa.Chat.Completion.CompletionSession, withHistory?: Marisa.Chat.Completion.CompletionSession[], sessionPutCallback?: () => void): Promise<any> {
-
+    public async put(
+        session: Marisa.Chat.Completion.CompletionSession, 
+        _withHistory?: Marisa.Chat.Completion.CompletionSession[], 
+        sessionPutCallback?: () => void
+    ): Promise<void> {
         this.addContextFunction?.(session)
+        
         if (sessionPutCallback) {
             sessionPutCallback()
         }
     }
 
-    public async query(userPrompt: string): Promise<[sessions: Marisa.Chat.Completion.CompletionSession[], promptAddition: string]> {
+    public async query(userPrompt: string): Promise<[Marisa.Chat.Completion.CompletionSession[], string]> {
         const userMessage: Marisa.Chat.Completion.Messages.ChatCompletionUserMessage = {
             content: userPrompt,
             role: 'user',
             timestamp: Date.now()
         }
+        
         const newSession = this.createEmptySession()
         newSession.messages.push(userMessage)
+        
         const hotSessionLength = this.options?.hotSessionLength ?? 5
         const keepHotSessionLength = this.options?.keepHotSessionLength ?? 3
-        const beforeSessions = this.filterSessions(hotSessionLength, keepHotSessionLength)
-        return [this.noSystemInject(beforeSessions), '']
+        const historySessions = this.filterSessions(hotSessionLength, keepHotSessionLength)
+        return [[...historySessions, newSession], '']
     }
 }
